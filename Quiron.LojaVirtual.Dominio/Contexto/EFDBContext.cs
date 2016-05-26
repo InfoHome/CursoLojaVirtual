1237c61dfcb893211e0cd2dafee9c3b58f404d39
tobiaskrt <tobiasitin@gmail.com.br>
2016-05-25 21:52:18 -0300

    EFDBContext

---
+++ Quiron.LojaVirtual.Dominio/Contexto/EFDBContext.cs
@@ -0,0 +1,28 @@
+﻿
+using Quiron.LojaVirtual.Dominio.Entidade;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Quiron.LojaVirtual.Dominio.Contexto
+{
+
+    public class EFDBContext : DbContext
+    {
+        public EFDBContext() : base("EFDBContext")
+        {
+
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            
+
+        }
+    }
+}
