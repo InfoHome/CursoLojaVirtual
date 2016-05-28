3f8e17816859026e21d36f5b802b9e522e3094ba
tobiaskrt <tobiasitin@gmail.com.br>
2016-05-28 16:47:30 -0300

    Layout2

---
+++ Quiron.LojaVirtual.Web/ViewModels/Paginacao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Quiron.LojaVirtual.Web.ViewModels
+{
+    public class Paginacao
+    {
+        public int ItensTotal { get; set; }
+        public int ItensPorPagina { get; set; }
+        public int PaginaAtual { get; set; }
+        public int TotalDePagina
+        {
+            get
+            {
+                return (int)Math.Ceiling((decimal)ItensTotal / ItensPorPagina);
+            }
+        }
+    }
+}
